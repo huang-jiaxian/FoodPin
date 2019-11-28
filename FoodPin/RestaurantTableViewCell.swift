265a46d11d520435b5c7cd07653c99e7d2192959
jia <417673957@qq.com>
2019-11-28 10:29:00 +0800

    before pull

---
+++ FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by jia on 2019/11/13.
+//  Copyright © 2019 jia. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    
+    // declare the variables,outlet : 输出
+    @IBOutlet var nameLabel:UILabel!
+    @IBOutlet var locationLabel:UILabel!
+    @IBOutlet var typeLabel:UILabel!
+    @IBOutlet var thumbnailImageView : UIImageView!
+    @IBOutlet var heartImageView : UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
