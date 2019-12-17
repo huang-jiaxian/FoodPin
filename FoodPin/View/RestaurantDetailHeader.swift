5724b8b3aa556a4b6d3978cd644f3ac8f4c7c804
jia <417673957@qq.com>
2019-12-17 18:22:49 +0800

    fix the problem of the imgage's mode

--- FoodPin/View/RestaurantDetailHeader.swift
+++ FoodPin/View/RestaurantDetailHeader.swift
@@ -18,7 +18,11 @@ class RestaurantDetailHeader: UIView {
     }
     */
     @IBOutlet var headerImageView: UIImageView!
-    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!{
+        didSet {
+            nameLabel.numberOfLines = 0
+        }
+    }
     @IBOutlet var typeLabel: UILabel!
     @IBOutlet var heartImageView: UIImageView!
 
