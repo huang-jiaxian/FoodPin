a4919cab3e4ae47e5f25f224216b896314667082
huang-jiaxian <417673957@qq.com>
2019-12-20 01:30:32 +0800

    update
    更新table body

--- FoodPin/Controller/RestaurantDetailViewController.swift
+++ FoodPin/Controller/RestaurantDetailViewController.swift
@@ -8,7 +8,46 @@
 
 import UIKit
 
-class RestaurantDetailViewController: UIViewController {
+class RestaurantDetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        switch indexPath.row {
+        
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailIconTextCell.self), for: indexPath) as! RestaurantDetailIconTextCell
+            cell.iconImageView.image = UIImage(systemName: "phone")?.withTintColor(.black,renderingMode:  .alwaysOriginal)
+            cell.shortTextLabel.text = restaurant.phone
+            cell.selectionStyle = .none
+            
+            return cell
+            
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailIconTextCell.self), for: indexPath) as! RestaurantDetailIconTextCell
+            cell.iconImageView.image = UIImage(systemName: "map")?.withTintColor(.black,renderingMode:  .alwaysOriginal)
+            cell.shortTextLabel.text = restaurant.location
+            cell.selectionStyle = .none
+            
+            return cell
+            
+        case 2:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailTextCell.self), for: indexPath) as! RestaurantDetailTextCell
+            cell.descriptionLabel.text = restaurant.description
+            cell.selectionStyle = .none
+            
+            return cell
+            
+        default:
+            fatalError("Failed to instantiate the table view cell for detail view controller")
+        }
+    }
+    
     
 //    @IBOutlet var restaurantImageView: UIImageView!
 //    @IBOutlet var nameLabel: UILabel!
@@ -41,14 +80,21 @@ class RestaurantDetailViewController: UIViewController {
 //        typeLabel.text = restaurant.type
 //        phoneLabel.text = restaurant.phone
 //        descriptionLabel.text = restaurant.description
-        
+      
         // 大标题不再出现
         navigationItem.largeTitleDisplayMode = .never
         
+        // 来源于tableviewde的prepare
         headerView.headerImageView.image = UIImage(named: restaurant.name)
         headerView.nameLabel.text = restaurant.name
         headerView.typeLabel.text = restaurant.type
         headerView.heartImageView.isHidden = restaurant.isVisited ? false : true
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        
+        
     }
     
 
--- FoodPin/View/RestaurantDetailHeader.swift
+++ FoodPin/View/RestaurantDetailHeader.swift
@@ -21,6 +21,7 @@ class RestaurantDetailHeader: UIView {
     @IBOutlet var nameLabel: UILabel! {
         didSet {
             nameLabel.numberOfLines = 0
+        
         }
     }
     @IBOutlet var typeLabel: UILabel! {
@@ -29,7 +30,12 @@ class RestaurantDetailHeader: UIView {
             typeLabel.layer.masksToBounds = true
         }
     }
-    @IBOutlet var heartImageView: UIImageView!
+    @IBOutlet var heartImageView: UIImageView! {
+        didSet {
+            heartImageView.image = UIImage(named: "heart-tick")?.withRenderingMode(.alwaysTemplate)
+            heartImageView.tintColor = .white
+        }
+    }
 
     var restaurant = Restaurant()
     
