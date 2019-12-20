9335ae7b717b217f052d76248ef93f2b84b0bf73
jia <417673957@qq.com>
2019-12-20 18:32:48 +0800

    change the navigation's style

--- FoodPin/Controller/RestaurantTableViewController.swift
+++ FoodPin/Controller/RestaurantTableViewController.swift
@@ -56,6 +56,12 @@ class RestaurantTableViewController: UITableViewController {
         
         // largr title
         navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        if let customFont = UIFont(name: "Rubik-Medium", size: 40.0) {
+            navigationController?.navigationBar.largeTitleTextAttributes = [ NSAttributedString.Key.foregroundColor: UIColor(red: 231.0/255.0, green: 76.0/255.0, blue:60.0/255.0, alpha: 1.0), NSAttributedString.Key.font: customFont ]
+        }
+        
     }
 
     // MARK: - Table view data source
